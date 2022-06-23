3b11c0d240c4adceec62f74d5a0ceb651c517251
DimaZh <dima.hh21@gmail.com>
2022-06-23 15:17:29 +0300

    first commit

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>About Web-Site "Question"</h1>
+  <img alt="logo-image" src="../assets/logo.jpg" class="rounded mb-3" />
+  <p>This site was created for the opportunity to conduct surveys.</p>
+  <p>It was created by Zhovnirskyi Dmytro KV-11mp</p>
+  <p>
+    This site was created for the opportunity to conduct a survey and looks like
+    a question with predefined answers. Only registered users can create polls
+    and respond.
+  </p>
+  <p>
+    This site can be used to collect statistics on the things you need, for work
+    or just for the sake of interest. Only the site administrator can delete the
+    question.
+  </p>
+  <p>Admin user has possibility to see all users and delete any polling</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AboutPage",
+});
+</script>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div v-if="$store.state.currentUser">
+      <router-link
+        to="/create-question"
+        custom
+        v-slot="{ navigate, isActive, isExactActive }"
+      >
+        <button
+          id="create-question-btn"
+          role="link"
+          class="btn btn-primary mb-3"
+          @click="navigate"
+          :class="{ active: isActive, 'exact-active': isExactActive }"
+        >
+          Create new polling
+        </button>
+      </router-link>
+    </div>
+    <div v-else>
+      <div class="alert alert-warning" role="alert">
+        You must first register in order to be able to participate in the survey
+        and ask questions yourself.
+      </div>
+    </div>
+    <QuestionList :questions="$store.state.questions.questionList" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import QuestionList from "@/components/QuestionList.vue";
+
+export default defineComponent({
+  name: "HomePage",
+  components: { QuestionList },
+});
+</script>
